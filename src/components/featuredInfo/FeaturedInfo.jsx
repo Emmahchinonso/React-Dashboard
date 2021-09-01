7f63df970527fa2542010b497b73f61ed49afbde
chinonso <emmanuelchinons9@gmail.com>
2021-09-01 23:50:17 +0100

    redo the readme

--- src/components/featuredInfo/FeaturedInfo.jsx
+++ src/components/featuredInfo/FeaturedInfo.jsx
@@ -7,30 +7,37 @@ function FeaturedInfo() {
         <div className={styles.featured}>
             <div className={styles.featuredItem}>
                 <span className={styles.featuredTitle}>Revenue</span>
-                <div className={styles.featuredMoneyConteiner}>
+                <div className={styles.featuredMoneyContainer}>
                     <span className={styles.featuredMoney}>$2,415</span>
                     <span className={styles.featuredMoneyRate}>
-                        -11.4 <ArrowDownward />
+                        -11.4{" "}
+                        <ArrowDownward
+                            className={`${styles.featuredIcon} ${styles.negative}`}
+                        />
                     </span>
                 </div>
                 <div className={styles.featuredSub}>Compare to last month</div>
             </div>
             <div className={styles.featuredItem}>
                 <span className={styles.featuredTitle}>Sales</span>
-                <div className={styles.featuredMoneyConteiner}>
+                <div className={styles.featuredMoneyContainer}>
                     <span className={styles.featuredMoney}>$4,415</span>
                     <span className={styles.featuredMoneyRate}>
-                        -1.4 <ArrowDownward />
+                        -1.4{" "}
+                        <ArrowDownward
+                            className={`${styles.featuredIcon} ${styles.negative}`}
+                        />
                     </span>
                 </div>
                 <div className={styles.featuredSub}>Compare to last month</div>
             </div>
             <div className={styles.featuredItem}>
                 <span className={styles.featuredTitle}>Cost</span>
-                <div className={styles.featuredMoneyConteiner}>
+                <div className={styles.featuredMoneyContainer}>
                     <span className={styles.featuredMoney}>$2,225</span>
                     <span className={styles.featuredMoneyRate}>
-                        -11.4 <ArrowUpward />
+                        +2.4{" "}
+                        <ArrowUpward className={`${styles.featuredIcon} `} />
                     </span>
                 </div>
                 <div className={styles.featuredSub}>Compare to last month</div>
